228c8f32c4bf2001c7c6e01f189b0e73e00591b0
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-28 03:10:12 +0600

    added heart count functionality

---
+++ scripts/script.js
@@ -0,0 +1,65 @@
+
+document.getElementById('emergency-services-hearts1').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('emergency-services-hearts1')
+    .classList.add("emergency-hearts");
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts2').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts3').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts4').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts5').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts6').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts7').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts8').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
+
+document.getElementById('emergency-services-hearts9').addEventListener('click', function () {
+    const navHeartLogoCount = parseInt(document.getElementById('nav-heart-logo-count').innerText);
+    const updateValue = navHeartLogoCount + 1;
+    document.getElementById('nav-heart-logo-count')
+        .innerText = updateValue;
+})
