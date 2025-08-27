9c5ff14b56c70bbd838a731d1ecad73949f2fcb6
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-28 03:43:26 +0600

    added copy functionality and on progress

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,1 +1,63 @@
-console.log("copy connected");
+document.getElementById('copy-btn1')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn2')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn3')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn4')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn5')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn6')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn7')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn8')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
+document.getElementById('copy-btn9')
+    .addEventListener('click', function () {
+        const copyCount = parseInt(document.getElementById('copy-count').innerText);
+        const updateValue = copyCount + 1;
+        document.getElementById('copy-btn1').classList.add("copy-btn");
+        document.getElementById('copy-count').innerText = updateValue;
+    })
